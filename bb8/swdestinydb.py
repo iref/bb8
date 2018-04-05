bc81a343c2575fe441f6c01a42b2a7541a1a673c
Jan Ferko <julyloov@gmail.com>
2018-04-05 14:27:14 +0200

    Complete SWDestinyDB client

--- bb8/swdestinydb.py
+++ bb8/swdestinydb.py
@@ -1,29 +1,102 @@
+# -*- coding: utf-8 -*-
+"""SWDestinyDB handling module.
+
+This module provides utilities for communication with SWDestinyDB API.
+At the moment, it only supports public endpoints.
+
+Example:
+    A short usage example of getting card by its key::
+    
+        $ client = SWDestinyClient()
+        $ client.get_card("01001")
+"""
 import requests
 
 
-BASE_URL = "https://swdestinydb.com/api/public"
-
-
 class SWDestinyDBClient:
+    """API client for StarWars Destiny DB.
 
-    def __init__(self, base_url, format="json"):
-        self.base_url = base_url
+    Supports only publicly available endpoints.
+    See https://swdestinydb.com/api/docs for more details.
+    
+    Args:
+        base_url (string): The url where api resources can be found. 
+            (default: https://swdestinydb.com/api/public)
+        format (string): The expected output format. (default: json)
+        session (obj): The HTTP session to communicate with API (default: requests.Session())
+    """
+
+    def __init__(self, base_url=None, format="json", session=None):
+        self.base_url = base_url or "https://swdestinydb.com/api/public"
         self.format = format
+        self.session = session or requests.Session()
 
-    def get_card(self, key, format="json"):
-        pass
+    def get_card(self, key):
+        """Gets card with given key.
+
+        See https://swdestinydb.com/api/doc#get--api-public-card-{card_code}.{_format} for more details.
+
+        Args:
+            key (string): The card identifier
+
+        Returns:
+            API response object
+        """
+        uri = "{}/card/{}.{}".format(self.base_url, key, self.format)
+        return self.session.get(uri)
 
     def get_cards(self, set_code=None):
-        pass
+        """Gets all cards.
+
+        If `set_code` is provided, it only returns cards from given set.
+        See https://swdestinydb.com/api/doc#get--api-public-cards- for more details.
+
+        Args:
+            set_code (string): The code of set cards should be from, e.g 'AW'. (default: None)
+
+        Returns:
+            API response object
+        """
+        set_path = ""
+        if set_code:
+            set_path = set_code + "." + self.format
+
+        uri = "{}/cards/{}".format(self.base_url, set_path)
+        return self.session.get(uri)
 
     def get_decklist(self, key):
-        pass
+        """Gets decklist by its identifier.
 
-    def get_decklists(self, date):
-        pass
+        See https://swdestinydb.com/api/doc#get--api-public-decklist-{decklist_id}.{_format} for more details.
+
+        Args:
+            key (string): The decklist identifier
+
+        Returns:
+            API response object
+        """
+        uri = "{}/decklist/{}.{}".format(self.base_url, key, self.format)
+        return self.session.get(uri)
 
     def get_formats(self):
-        pass
+        """Gets all available competitive formats.
+
+        See https://swdestinydb.com/api/doc#get--api-public-formats- for more details.
+
+        Returns:
+            API response object 
+        """
+        uri = "{}/formats/".format(self.base_url)
+        return self.session.get(uri)
 
     def get_sets(self):
-        pass
+        """Gets all available sets.
+
+        See https://swdestinydb.com/api/doc#get--api-public-sets- for more details.
+
+        Returns:
+            API response object
+        """
+        params = {"_format": self.format}
+        uri = "{}/sets/".format(self.base_url)
+        return self.session.get(uri, params=params)
--- tests/swdestinydb_test.py
+++ tests/swdestinydb_test.py
@@ -1,33 +1,72 @@
-from bb8.swdestinydb import BASE_URL, SWDestinyDBClient
+# -*- coding: utf-8 -*-
+from bb8.swdestinydb import SWDestinyDBClient
 
 import pytest
 
 pytest.mark.usefixtures('betamax_recorder')
 
 @pytest.fixture
-def client():
-    return SWDestinyDBClient(BASE_URL, "json")
+def client(betamax_session):
+    return SWDestinyDBClient(session=betamax_session)
 
-def test_get_all_cards(client, betamax_recorder):
-    with betamax_recorder.use_cassette("swdestinydb_cards"):
-        resp = client.get_cards()
+def test_get_all_cards(client):
+    resp = client.get_cards()
 
     assert resp.status_code == 200
     assert len(resp.json()) > 0
 
 
-def test_get_card(client, betamax_recorder):
-    with betamax_recorder.use_cassette("swdestinydb_card"):
-        resp = client.get_card("01001")
+def test_get_all_sets_cards(client):
+    resp = client.get_cards(set_code="AW")
 
     assert resp.status_code == 200
-    print(resp.text)
+    
+    for card in resp.json():
+        card_set = card["set_code"]
+        assert card_set == "AW", "Found card from invalid set. Set: {}".format(card_set)
+
+
+def test_get_card(client):
+    resp = client.get_card("01001")
+
+    assert resp.status_code == 200
 
     data = resp.json()
-    assert data["title"] == "Captain Phasma"
+    assert data["name"] == "Captain Phasma"
     assert data["set_code"] == "AW"
     assert data["type_code"] == "character"
     assert data["faction_code"] == "red"
     assert data["affiliation_code"] == "villain"
     assert data["rarity_code"] == "L"
     assert data["code"] == "01001"
+
+
+def test_get_decklist(client):
+    resp = client.get_decklist(19504)
+
+    assert resp.status_code == 200
+
+    data = resp.json()
+    assert data["name"] == "Make Jar Jar Good Again!"
+    assert data["affiliation_code"] == "hero"
+    assert len(data["characters"]) == 3
+
+
+def test_get_formats(client):
+    resp = client.get_formats()
+
+    assert resp.status_code == 200
+
+    data = resp.json()
+    codes = [format["code"] for format in data]
+
+    assert "STD" in codes
+    assert "TRI" in codes
+    assert "INF" in codes
+
+
+def test_get_sets(client):
+    resp = client.get_sets()
+
+    assert resp.status_code == 200
+    assert len(resp.json()) > 0
