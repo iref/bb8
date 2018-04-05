0f960d5a10f6ab98caa4fcacc998fe6a351eb132
Jan Ferko <julyloov@gmail.com>
2018-04-05 16:04:35 +0200

    Fix betamax record mode
    Betamax used nonexistent record mode in CI builds, which caused test job
    to fail miserably.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,9 @@
 import betamax
 import os
 
-record_mode = "always" if os.environ.get("CI") else "once"
+record_mode = "all" if os.environ.get("GITLAB_CI") else "once"
 
 with betamax.Betamax.configure() as config:
     config.cassette_library_dir = "tests/cassettes"
+    print(record_mode)
     config.default_cassette_options["record_mode"] = record_mode
